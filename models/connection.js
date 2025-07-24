2deea1dcdd7a83bfc898c73ee3b3e9917ef5430e
Ajayraj1113 <ajayraj824@gmail.com>
2025-07-24 11:23:49 +0530

    user added successfully

--- models/connection.js
+++ models/connection.js
@@ -1,14 +1,12 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("Database connected Successfully!!")
-        
-    }
-    catch(error) {
-        console.log("Error : ", error.errmsg)
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected Successfully!!");
+  } catch (error) {
+    console.log("Database connection Error : ", error.errmsg);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
